b09453870c1d8da983e1338646e3865dd88b1e61
larry <giandny@gmail.com>
2014-12-28 10:14:31 +0100

    initial

---
+++ KarthusSupport/KarthusSupport/KarthusSupport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace KarthusSupport
+{
+    public class KarthusSupport
+    {
+        static void Main(string[] args)
+        {
+            new KarthusSupport();
+        }       
+
+        public KarthusSupport()
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private void Game_OnGameLoad(EventArgs args)
+        {
+            Game.PrintChat("KarthusSupport - Loaded");
+        }
+    }
+}
